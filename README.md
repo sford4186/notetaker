b266148f6793ead5512d8d64dbf220e6a60ac7c1
Shawanda Ford <shawanda.ford@gmail.com>
2020-10-01 19:53:11 -0400

    updated comments on server.js file

--- README.md
+++ README.md
@@ -3,14 +3,15 @@
 ##  Description
 This is an application that can be used to write, save, and delete notes using express on the backend to save and retrieve note data from a JSON file.  Technologies used: HTML, JQuery, Node.js, Express.js.
 
-## Screenshot of website  
-![Screenshot of deployed site](#.PNG)
-
-![Video Tutorial][README Creation Video](https://drive.google.com/file/d/1OeR3uCoW-9dXI1WafmXVyq3L7CAMN6Aj/view) 
+## Screenshot of Website  
+![Screenshot of deployed site](screenshot.png)
 
 ## Github Repository
-
 [notetaker](https://github.com/sford4186/notetaker)
+
+## Deployed Site
+https://secret-chamber-25028.herokuapp.com/
+
 
 
  
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 //link express to the public directory to access html and js files
 app.use(express.static("public"));
 
-//set array to store all of the notes
+//importing db.json and storing it in a variable called notes
 var notes = require('./db/db.json')
 
 // Routes
@@ -33,19 +33,9 @@ app.get("/notes", function (req, res) {
 });
 
 
-//   app.get("/api/notes", function(req, res) {
-//     return res.sendFile(path.json(__dirname, "Develop/db/db.json"));
-//   });
-
-
 app.get("/api/notes", function (err, res) {
 
-  // // reads the notes from db.json file
-  // notes = fs.readFileSync("Develop/db/db.json", "utf8");
-  // // parse it so notes is an array of objects
-  // notes = JSON.parse(notes);
-
-  //send objects to the browser
+  // // reads the notes from db.json file and sends object of notes to the browser
   res.json(notes);
 });
 
@@ -53,31 +43,33 @@ app.get("/api/notes", function (err, res) {
 // writes the new note to the json file
 app.post("/api/notes", function (req, res) {
   if(notes.length){
+    //assign a unique id to each note entered in by the user
     req.body.id = notes[notes.length-1].id + 1
   }else{
     req.body.id =1;
   }
-  // Set new notes id
+  
   // add the new note to the array of note objects
-
-  //req.body = user input???
+  //req.body = user input
   notes.push(req.body);
   // make it string(stringify)so you can write it to the file
-  //JSON.stringify(notes)
   // writes the new note to file
-console.log(notes)
+  console.log(notes)
   fs.writeFile("./db/db.json", JSON.stringify(notes),  function (err) {
     // error handling
     if (err) throw err;
+
+    //status = "OK"
     res.sendStatus(200);
   });
-  // changeit back to an array of objects & send it back to the browser(client)
+  
 });
 
-// Delete a note???
+// Delete a note
 
 app.delete("/api/notes/:id", function (req, res) {
   console.log(req.params.id)
+  //find the note with the delete id
   notes = notes.filter(note=>note.id !== parseInt(req.params.id))
   
   fs.writeFile("./db/db.json", JSON.stringify(notes),  function (err) {
@@ -87,9 +79,6 @@ app.delete("/api/notes/:id", function (req, res) {
   });
 });
 
-//read the file
-//find the note with the delete id
-// res.send(JSON.parse(notes));
 // default to home page
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "Develop/public/index.html"));
